2af9cc1890e4d9c17153a788d81584dcb85d8840
prafalex <andrei.praf@s.unibuc.ro>
2022-04-16 23:30:45 +0300

    truffle-v2

--- cars_project/truffle-config.js
+++ cars_project/truffle-config.js
@@ -18,95 +18,95 @@
  *
  */
 
-const  HDWalletProvider = require("truffle-hdwallet-provider");
-const LoomTruffleProvider = require('loom-truffle-provider');
-
-
-const mnemonic = "soldier alien sound usual when slide almost clever swift monster plunge since";
-
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
-module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
-  networks: {
-    // Configuration for mainnet
-    mainnet: {
-      provider: function () {
-        // Setting the provider with the Infura Mainnet address and Token
-        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/0a665a91f77c4a20b54becbd57d2573a")
-      },
-      network_id: "1"
-    },
-    // Configuration for rinkeby network
-    rinkeby: {
-      // Special function to setup the provider
-      provider: function () {
-        // Setting the provider with the Infura Rinkeby address and Token
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/0a665a91f77c4a20b54becbd57d2573a")
-      },
-      network_id:4 //Fill in the `network_id` for the Rinkeby network.
-    },
-    loom_testnet: {
-      provider: function() {
-        const privateKey = 'YOUR_PRIVATE_KEY'
-        const chainId = 'extdev-plasma-us1';
-        const writeUrl = 'http://extdev-plasma-us1.dappchains.com:80/rpc';
-        const readUrl = 'http://extdev-plasma-us1.dappchains.com:80/query';
-        return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
-        },
-      network_id: '9545242630824'
-    }
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
-  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
-  // }
-};
+ const  HDWalletProvider = require("truffle-hdwallet-provider");
+ const LoomTruffleProvider = require('loom-truffle-provider');
+ 
+ 
+ const mnemonic = "soldier alien sound usual when slide almost clever swift monster plunge since";
+ 
+ //
+ // const fs = require('fs');
+ // const mnemonic = fs.readFileSync(".secret").toString().trim();
+ 
+ module.exports = {
+   /**
+    * Networks define how you connect to your ethereum client and let you set the
+    * defaults web3 uses to send transactions. If you don't specify one truffle
+    * will spin up a development blockchain for you on port 9545 when you
+    * run `develop` or `test`. You can ask a truffle command to use a specific
+    * network from the command line, e.g
+    *
+    * $ truffle test --network <network-name>
+    */
+ 
+   networks: {
+     // Configuration for mainnet
+     mainnet: {
+       provider: function () {
+         // Setting the provider with the Infura Mainnet address and Token
+         return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/0a665a91f77c4a20b54becbd57d2573a")
+       },
+       network_id: "1"
+     },
+     // Configuration for rinkeby network
+     rinkeby: {
+       // Special function to setup the provider
+       provider: function () {
+         // Setting the provider with the Infura Rinkeby address and Token
+         return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/0a665a91f77c4a20b54becbd57d2573a")
+       },
+       network_id:4 //Fill in the `network_id` for the Rinkeby network.
+     },
+     loom_testnet: {
+       provider: function() {
+         const privateKey = 'YOUR_PRIVATE_KEY'
+         const chainId = 'extdev-plasma-us1';
+         const writeUrl = 'http://extdev-plasma-us1.dappchains.com:80/rpc';
+         const readUrl = 'http://extdev-plasma-us1.dappchains.com:80/query';
+         return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+         },
+       network_id: '9545242630824'
+     }
+   },
+ 
+   // Set default mocha options here, use special reporters etc.
+   mocha: {
+     // timeout: 100000
+   },
+ 
+   // Configure your compilers
+   compilers: {
+     solc: {
+       version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       //  optimizer: {
+       //    enabled: false,
+       //    runs: 200
+       //  },
+       //  evmVersion: "byzantium"
+       // }
+     }
+   },
+ 
+   // Truffle DB is currently disabled by default; to enable it, change enabled:
+   // false to enabled: true. The default storage location can also be
+   // overridden by specifying the adapter settings, as shown in the commented code below.
+   //
+   // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+   // make a backup of your artifacts to a safe location before enabling this feature.
+   //
+   // After you backed up your artifacts you can utilize db by running migrate as follows: 
+   // $ truffle migrate --reset --compile-all
+   //
+   // db: {
+     // enabled: false,
+     // host: "127.0.0.1",
+     // adapter: {
+     //   name: "sqlite",
+     //   settings: {
+     //     directory: ".db"
+     //   }
+     // }
+   // }
+ };
